f6d20bb99dbd831d79fbc07eea3fc278eb76ba0b
InacioCarvalhoOliveira <inacio.carvalho.oliveira02@gmail.com>
2024-08-30 08:59:32 -0300

    first commit

---
+++ src/test/java/br/com/inaciooliveira/desafiotodolist/DesafiotodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.inaciooliveira.desafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiotodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
